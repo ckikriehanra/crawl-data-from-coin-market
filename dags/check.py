76d0053c8577750b653e86cee6abe1ff9dbf300b
nghiacken <64877009+ckikriehanra@users.noreply.github.com>
2023-05-25 16:37:32 +0700

    Update check.py

--- dags/check.py
+++ dags/check.py
@@ -1,7 +1,7 @@
 from airflow import DAG 
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
-from datetime import datetime
+from datetime import datetime, timedelta
 
 spark_master = "spark://spark:7077"
 spark_app_name = "check"
@@ -18,7 +18,7 @@ with DAG("check", start_date=datetime(2023,1,1), schedule_interval="@once", catc
         conn_id="spark_default",
         verbose=1,
         conf={"spark.master":spark_master},
-        jars="/usr/local/spark/resources/jars/aws-java-sdk-bundle-1.11.972.jar,/usr/local/spark/resources/jars/hadoop-aws-3.3.1.jar",
+        jars="/usr/local/spark/resources/jars/aws-java-sdk-bundle-1.11.972.jar,/usr/local/spark/resources/jars/hadoop-aws-3.3.1.jar"
     )
 
     start >> check >> end
