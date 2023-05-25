1c79a87193a544b2b9bf446d475ab7f889941f13
nghiacken <64877009+ckikriehanra@users.noreply.github.com>
2023-05-25 16:42:46 +0700

    Update _upload_data_into_fact_price_hour.py

--- spark/app/_upload_data_into_fact_price_hour.py
+++ spark/app/_upload_data_into_fact_price_hour.py
@@ -1,20 +1,17 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-import sys
-from pyspark.sql.functions import col, abs, hash, to_date
-    
+from pyspark.sql.functions import col, hour, lit, avg
 
+import sys
+import datetime
 
 if __name__ == "__main__":
-    # Read data from bronze layer
-    file_name_in_parquet = sys.argv[1]  
-    bucket_name = sys.argv[2]
-    dim_symbol_path = sys.argv[3]
-    dim_date_path = sys.argv[4]
-    fact_minute_price = sys.argv[5]
-    ACCESS_KEY = sys.argv[6]
-    SECRET_KEY = sys.argv[7]
-    ENDPOINT = sys.argv[8]
+    bucket_name = sys.argv[1]
+    fact_price_hour_path = sys.argv[2]
+    fact_price_minute_path = sys.argv[3]
+    ACCESS_KEY = sys.argv[4]
+    SECRET_KEY = sys.argv[5]
+    ENDPOINT = sys.argv[6]
 
     # Create a SparkSession
     spark = SparkSession.builder \
@@ -26,96 +23,42 @@ if __name__ == "__main__":
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
     .getOrCreate()
 
-    schema = StructType([
-        StructField("time", TimestampType(), True),
-        StructField("symbol", StringType(), False),
-        StructField("name", StringType(), False),
-        StructField("price", FloatType(), True),
-        StructField("1h%", FloatType(), True),
-        StructField("24h%", FloatType(), True),
-        StructField("7d%", FloatType(), True),
-        StructField("market_cap", FloatType(), True),
-        StructField("volume_24h", FloatType(), True),
-        StructField("circulating_supply", FloatType(), True)
-    ])
 
-    schema_dim_date = StructType([
-        StructField("Date_key", IntegerType(), True),
-        StructField("TheDate", DateType(), True),
-        StructField("TheDay", IntegerType(), True),
-        StructField("TheDayName", StringType(), True),
-        StructField("TheWeek", IntegerType(), True),
-        StructField("TheDayOfWeek", IntegerType(), True),
-        StructField("TheMonth", StringType(), True),
-        StructField("TheMonthName", StringType(), True),
-        StructField("TheQuarter", IntegerType(), True),
-        StructField("TheYear", IntegerType(), True),
-        StructField("TheMonthYear", StringType(), True),
-        StructField("TheYearMonth", StringType(), True),
-        StructField("TheQuarterYear", StringType(), True),
-        StructField("TheYearQuater", StringType(), True),
-        StructField("TheFirstOfMonth", DateType(), True),
-        StructField("TheLastOfYear", DateType(), True)
-    ])
- 
-    schema_dim_symbol = StructType([
-        StructField("symbol_key", IntegerType(), False),
-        StructField("symbol", StringType(), True),
-        StructField("name", StringType(), True)
-    ]) 
-
-
-        
-    df_parquet = spark.read \
+    df = spark.read \
         .format("parquet") \
-        .schema(schema) \
-        .load("s3a://{}/{}".format(bucket_name, file_name_in_parquet))
+        .load("s3a://{}/{}".format(bucket_name, fact_price_minute_path))
     
-    df_dim_date = spark.read \
-        .format("parquet") \
-        .schema(schema_dim_date) \
-        .load("s3a://{}/{}".format(bucket_name, dim_date_path))
+    # df.show()
+    
+    now = datetime.datetime.now() + datetime.timedelta(hours=7)
+    
+    curr_hour = now.hour
+    curr_date = now.date()
+    # prev_hour = curr_hour
+    prev_hour = curr_hour-1
+    if prev_hour < 0:
+        prev_hour += 24
+        curr_date = curr_date - datetime.timedelta(days=1)
 
-    # Create key
-    df_temp = (df_parquet.withColumn("symbol_key", (abs(hash(col("symbol"))) % 100000000))).select("symbol_key", "symbol", "name")
+    newest_records_hour_day = df.filter((hour(col("time")) == prev_hour) & (col("time").cast("date") == curr_date)) \
+        .withColumn("hour", lit(prev_hour)) \
+        .withColumn("day", lit(curr_date)) \
+        .select("symbol_key", "hour", "day", "price", "volume_24h") \
+        .groupBy("symbol_key", "hour", "day") \
+        .agg(avg("price").alias("avg_price_by_hour"), avg("volume_24h").alias("avg_volume_24h_by_hour"))
     
-    # Load exist dim_symbol
-    df_exist = spark.createDataFrame(spark.sparkContext.emptyRDD(), schema_dim_symbol)
+    # newest_records_hour_day.show()
+    
     try:
-        df_exist = spark.read \
-            .format("parquet") \
-            .schema(schema_dim_symbol) \
-            .load("s3a://{}/{}".format(bucket_name, dim_symbol_path))
-    except:
-        pass
-    
-    # Update dim_symbol
-    df_result = df_exist.union(df_temp).distinct()
-    
-    # Load data into fact table
-    df_fact = (df_result.join(df_parquet, (df_parquet.symbol==df_result.symbol) & (df_parquet.name==df_result.name), how="inner")) \
-        .join(df_dim_date, to_date(col("time"))==df_dim_date.TheDate, how="inner") \
-        .select ("symbol_key", "Date_key", "time", "price", "1h%", "24h%", "7d%", "market_cap", "volume_24h", "circulating_supply")
-
-    # Update data in gold layer
-    # Update fact_hour_exchange_rate
-    try :
-        df_fact.write \
-            .format("parquet") \
-            .mode("append") \
-            .save("s3a://{}/{}".format(bucket_name, fact_minute_price))
+        newest_records_hour_day.write\
+            .format("parquet")\
+            .mode("append")\
+            .save("s3a://{}/{}".format(bucket_name, fact_price_hour_path))
     except FileNotFoundError:
-        df_fact.write \
-        .format("parquet") \
-        .mode("overwrite") \
-        .save("s3a://{}/{}".format(bucket_name, fact_minute_price))
-
-    # Update dim_symbol
-    df_result.write \
-        .format("parquet") \
-        .mode("overwrite") \
-        .save("s3a://{}/{}".format(bucket_name, dim_symbol_path))
-    
-    
-    # Stop the SparkSession
+        newest_records_hour_day.write\
+            .format("parquet")\
+            .mode("overwrite")\
+            .save("s3a://{}/{}".format(bucket_name, fact_price_hour_path))
     spark.stop()
+    
+    
